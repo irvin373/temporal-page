24bbeabe6e3c70e44485618c234f2b8e1831d74b
Luis_Prowess <herrera.dev1898@gmail.com>
2025-11-14 06:07:35 -0300

    branch for the integration of React and form enhancement features.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://irvin373.github.io',
@@ -10,6 +12,7 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
+  integrations: [react()], // Integración de React
   server: {
     open: true, // 🚀 Esto hace que se abra el navegador automáticamente
   },
