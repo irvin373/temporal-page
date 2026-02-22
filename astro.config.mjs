2b5f2d8fcb9e263e31c38fd846d3fa8f672b2194
irvin373 <irvin373@gmail.com>
2026-02-22 17:34:54 -0400

    add React

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,20 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://irvin373.github.io',
   base: '/temporal-page',
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   server: {
     open: true, // 🚀 Esto hace que se abra el navegador automáticamente
   },
+
+  integrations: [react()],
 });
